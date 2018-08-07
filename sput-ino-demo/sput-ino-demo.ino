37a7ea82798f1c837fdd36df7030f20a1901ff74
sadr0b0t <ii1i7@yandex.ru>
2018-08-07 17:03:18 +0300

    Example on int overflow + example on led_on_even

--- sput-ino-demo/sput-ino-demo.ino
+++ sput-ino-demo/sput-ino-demo.ino
@@ -4,13 +4,20 @@
 void setup() {
     Serial.begin(9600);
     Serial.println("Just show that we call functions from tested lib, nothing useful here");
-}
-
-void loop() {
+    
+    pinMode(13, OUTPUT);
+    
     Serial.print("14+23=");
     Serial.println(a_plus_b(14, 23));
     Serial.print("14-23=");
     Serial.println(a_minus_b(14, 23));
+    Serial.print("34000+34000=");
+    Serial.println(a_plus_b(34000, 34000));
+}
+
+void loop() {
+    static int i = 0;
+    led_on_even(13, i++);
     delay(2000);
 }
 
